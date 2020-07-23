c8919b651902f946667ffde03ee150324192ebe0
Anish Tuli <tulianish@gmail.com>
2020-07-23 03:14:02 -0300

    Server side routing setup

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const path = require("path");
-
 const app = express();
+
+const postRoute = require('./api/routes/postRoute');
 
 // Serve the static files
 app.use(express.static(__dirname + "/affordly/build/"));
+
+app.use('/api',postRoute);
 
 app.get("/*", (req, res) => {
   //defining the path for the static files
   res.sendFile(path.join(__dirname + "/affordly/build/index.html"));
 });
 
-const port = process.env.PORT || 3000; // defining a port
+const port = process.env.PORT || 3001; // defining a port
 app.listen(port, () => {
   console.log("server is listening on :  ", port);
 });
