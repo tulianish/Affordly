995a22416ba44cb043c7064230f846b82b305adc
Anish Tuli <tulianish@gmail.com>
2020-07-25 01:16:15 -0300

    React build updated

---
+++ affordly/build/precache-manifest.9716bdac4511800089a426fd713f48bc.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae8abd9767eff13677a4",
+    "url": "/static/css/main.12980199.chunk.css"
+  },
+  {
+    "revision": "ae8abd9767eff13677a4",
+    "url": "/static/js/main.13e9729f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0a69ed8580369251001f",
+    "url": "/static/css/2.f77e8ca1.chunk.css"
+  },
+  {
+    "revision": "0a69ed8580369251001f",
+    "url": "/static/js/2.c0e1fd4c.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "a2d32687131f9e01a2c13ef412aa0cdf",
+    "url": "/static/media/a.a2d32687.jpg"
+  },
+  {
+    "revision": "771fa8152fe2a384e53a3430f8506ca4",
+    "url": "/static/media/b.771fa815.jpg"
+  },
+  {
+    "revision": "08c1c8d8d6afe1c04b808a9d6db8db01",
+    "url": "/static/media/c.08c1c8d8.jpg"
+  },
+  {
+    "revision": "63a58fbaf312cd0a03e3ee2d84242183",
+    "url": "/index.html"
+  }
+];
--- affordly/build/service-worker.js
+++ affordly/build/service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a25321a854e588616cc3433b0d83de67.js"
+  "/precache-manifest.9716bdac4511800089a426fd713f48bc.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
