0604ee842f94fca452f6c6dcd1ddc7d39e0b451e
guneet96 <gsgsdhillon@gmail.com>
2020-07-25 02:33:08 -0300

    route defs for server.js

--- server.js
+++ server.js
@@ -25,6 +25,14 @@ mongoose.connect(
 app.use("/support", supportRoutes);
 
 const postRoute = require('./api/Routes/postRoute');
+const current_user = require('./api/Routes/current_user');
+const login = require('./api/Routes/login');
+const user = require('./api/Routes/user');
+app.use("/api/current_user", current_user);
+app.use("/api/login", login);
+app.use("/api/user", user);
+app.use("/api", postRoute);
+
 
 // Serve the static files
 app.use(express.static(__dirname + "/affordly/build/"));
