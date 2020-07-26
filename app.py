c4e8f9807d646ad31bbc6b49d06f6eb304a9cd34
Rahul Anand <anand.rahul1205@gmail.com>
2020-07-25 23:31:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,33 @@
+
+"""
+ * Developed by-
+ *
+ * Name : Rahul Anand
+ * Banner ID : B00841310
+ * Email ID : rahul.anand@dal.ca
+ * 
+ """
 from flask import Flask, request, jsonify, render_template
 import requests
 from flask_cors import CORS,cross_origin
 import pymongo
 from bson.objectid import ObjectId
 
-
 app=Flask(__name__)
-CORS(app)
-mongo_client = pymongo.MongoClient("mongodb+srv://affordly:affordly123@cluster0.lzi2l.mongodb.net/affordly?retryWrites=true&w=majority")
+CsORS(app) # Allowed cross origin
 
+# Mongo connector
+mongo_client = pymongo.MongoClient("mongodb+srv://affordly:affordly123@cluster0.lzi2l.mongodb.net/affordly?retryWrites=true&w=majority")
 nosql_db = mongo_client["affordly"]
 col = nosql_db["posts"]
 
 
 @app.route('/clicked', methods=['GET'])
+    """
+    End point to increase number of clicks per view
+    -- Require param: post_id
+    -- Returns: Success or Exception
+    """
 def clicked():
     post_id=request.args['post_id']
     if not post_id:
@@ -31,16 +45,20 @@ def clicked():
             col.update_one({'_id':ObjectId(post_id)}, {'$set': {"clicks": clicks}})
         except Exception as e:
             print (str(e))
-            return "Some error occurred"
-    # col.update({}, {'$set': {"clicks": 0}}, multi=True)
-    
+            return "Some error occurred"    
     return "Success"
 
+
 @app.route('/trending', methods=['GET'])
+    """
+    End point to fetch trending items
+    -- Require param: None
+    -- Returns: List of id and images of trending items
+    """
 def trending():
     objects = col.find({"isActive":True}).sort([("clicks",-1)])
     return_dict = []
-    for x in objects:
+    for x in objects[:3]:
         idn = x.get('_id')
         image = x.get('img')
         return_dict.append({"id":str(idn), "image":image})
