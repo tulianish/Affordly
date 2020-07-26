7aa4f6bbd917aeed2e1a2ec59f69cf6c32f36365
Rahul Anand <anand.rahul1205@gmail.com>
2020-07-25 22:53:18 -0300

    Added trending feature

--- affordly/src/pages/home.js
+++ affordly/src/pages/home.js
@@ -5,12 +5,13 @@
  * Banner ID : B00844563
  * Email ID : piyush@dal.ca
  *
- * 
+ *
  * Feature Covered:
  * This is the frontend for our Home page.
- * 
+ *
  */
  // Modified by Anish Tuli (B00843522, anish.tuli@dal.ca)
+// Modified by Rahul Anand (B00841310, rahul.anand@dal.ca)
 
 import React from "react";
 import "react-dropdown/style.css";
@@ -20,16 +21,16 @@ import Carousel from "react-bootstrap/Carousel";
 import Footer from "../components/Footer";
 import Header from "../components/navbar";
 import "../stylesheets/home.css";
-import a from "../a.jpg";
-import b from "../b.jpg";
-import c from "../c.jpg";
 import axios from 'axios';
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      posts : []
+      posts : [],
+      trending1 : [],
+      trending2: [],
+      trending3 : []
     }
   }
 
@@ -37,7 +38,14 @@ class Home extends React.Component {
   let allPosts = axios.get("https://the-affordly.herokuapp.com/api/activePosts")
   .then(actPosts => actPosts)
   .then(data => this.setState({posts:data.data}));
-  }
+
+  let trendingPosts = axios.get("http://35.153.255.72/trending")
+  .then(trendPost => trendPost.data)
+  .then(data => {
+    console.log(data);
+    this.setState({trending1:data[0],trending2:data[1],trending3:data[2]})
+  });
+    }
 
   render() {
     return (
@@ -48,21 +56,21 @@ class Home extends React.Component {
           <div className="ctr">
             <Carousel>
               <Carousel.Item>
-                <img className="d-block w-100" src={a} alt="First slide" />
+                <a href={"/posting/"+this.state.trending1.id}><img className="d-block w-100" src={this.state.trending1.image} alt="First slide" /></a>
                 <Carousel.Caption>
                   <h2>Trendings</h2>
                   <p>Sell items on just a click.</p>
                 </Carousel.Caption>
               </Carousel.Item>
               <Carousel.Item>
-                <img className="d-block w-100" src={b} alt="Second slide" />
+                <a href={"/posting/"+this.state.trending2.id}><img className="d-block w-100" src={this.state.trending2.image} alt="Second slide" /></a>
                 <Carousel.Caption>
                   <h2>Trendings</h2>
                   <p>Buy items as you need.</p>
                 </Carousel.Caption>
               </Carousel.Item>
               <Carousel.Item>
-                <img className="d-block w-100" src={c} alt="Third slide" />
+                <a href={"/posting/"+this.state.trending3.id}><img className="d-block w-100" src={this.state.trending3.image} alt="Third slide" /></a>
                 <Carousel.Caption>
                   <h2>Trendings</h2>
                   <p>Items in your reach.</p>
@@ -102,7 +110,7 @@ class Home extends React.Component {
               </a>
             </div>
 )
-          })}  
+          })} 
           </div>
         </div>
 
